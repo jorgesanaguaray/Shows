a2b363d59aebc6a85df36e51fc5a3570af30ebff
jorgesanaguaray <jorgesanaguaray1@gmail.com>
2023-01-24 11:53:06 -0500

    Initial settings.

---
+++ app/src/main/java/com/jorgesanaguaray/shows/app/Shows.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.shows.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class Shows : Application()
