6f99e7a532885a88435be6b2a55e2a3de0f7a26f
jorgesanaguaray <jorgesanaguaray1@gmail.com>
2023-01-26 13:55:02 -0500

    I deleted unused attributes from ShowItem.

--- app/src/main/java/com/jorgesanaguaray/shows/domain/item/ShowItem.kt
+++ app/src/main/java/com/jorgesanaguaray/shows/domain/item/ShowItem.kt
@@ -9,29 +9,10 @@ import com.jorgesanaguaray.shows.data.remote.models.*
 data class ShowItem(
 
     val id: Int,
-    val url: String?,
-    val name: String?,
-    val type: String?,
-    val language: String?,
     val genres: List<String>?,
-    val status: String?,
-    val runtime: Int?,
-    val averageRuntime: Int?,
-    val premiered: String?,
-    val ended: String?,
     val officialSite: String?,
-    val schedule: Schedule?,
-    val rating: Rating?,
-    val weight: Int?,
-    val network: Network?,
-    val webChannel: WebChannel?,
-    val dvdCountry: DvdCountry?,
-    val externals: Externals?,
-    val image: Image?,
-    val summary: String?,
-    val updated: Int?,
-    val _links: Links?
+    val image: Image?
 
 )
 
-fun ShowModel.toShowItem() = ShowItem(id, url, name, type, language, genres, status, runtime, averageRuntime, premiered, ended, officialSite, schedule, rating, weight, network, webChannel, dvdCountry, externals, image, summary, updated, _links)
+fun ShowModel.toShowItem() = ShowItem(id, genres, officialSite, image)
